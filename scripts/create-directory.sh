4171ad3b10732c82786954c9b2725701a63ef6d2
Bryan Buchanan <bryan@resen.co>
2025-10-05 00:56:21 -0700

    Update create-directory.sh

--- scripts/create-directory.sh
+++ scripts/create-directory.sh
@@ -13,9 +13,20 @@
 # @raycast.author bryanbuchanan
 # @raycast.authorURL https://raycast.com/bryanbuchanan
 
-PREFIX="~/Library/CloudStorage/Dropbox-ERAMoto/ERA File Server"
+PREFIX="${HOME}/Library/CloudStorage/Dropbox-ERAMoto/ERA File Server"
 CLIPBOARD=$(pbpaste)
 CLIPBOARD=${CLIPBOARD//#/} # Remove '#' from order number, if present
 FULL_PATH="${PREFIX}/${CLIPBOARD#$PREFIX}" # Removes potential duplicate "prefixes" in directory path
+
+# Extract the first folder from clipboard path
+FIRST_FOLDER="${CLIPBOARD%%/*}"
+FIRST_FOLDER_PATH="${PREFIX}/${FIRST_FOLDER}"
+
+# Make sure the first folder of the copied path exists
+if [ ! -d "$FIRST_FOLDER_PATH" ]; then
+	echo "Directory doesn't exist"
+	exit 0
+fi
+
 mkdir -p "$(eval echo ${FULL_PATH})" && open "$(eval echo ${FULL_PATH})"
 
